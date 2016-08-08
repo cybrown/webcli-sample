63caf0beae5e52b57e402cdb1227f10a8f0df341
Cy Brown <cy.brown59@gmail.com>
2016-08-08 20:31:23 +0200

    Ajout d'une callback de configuration d'exemple

--- webcli.config.js
+++ webcli.config.js
@@ -1,3 +1,7 @@
 module.exports.configureWebpack = function (webpackConfig, webpack, mergeWebpackConfig) {
     // Overload webpack configuration here...
 };
+
+module.exports.configureKarma = function (karmaOptions, karma) {
+    // Overload karma configuration here...
+};
